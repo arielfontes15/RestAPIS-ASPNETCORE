3381850c7ff211c9803c76a7788cc1fe70303606
Ariel <arielfontes15@hotmail.com>
2022-01-15 20:26:18 -0300

    subindo

---
+++ RestAPICrud/RestAPICrud/Models/PessoaModel.cs
@@ -0,0 +1,11 @@
+﻿namespace RestAPICrud.Models
+{
+    public class PessoaModel
+    {
+        public long Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Endereco { get; set; }
+        public string Sexo { get; set; }
+    }
+}
