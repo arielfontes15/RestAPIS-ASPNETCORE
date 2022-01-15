aeb53e2cd6744e9636f803d2dff9d31c201e94e7
Ariel <arielfontes15@hotmail.com>
2022-01-15 20:12:59 -0300

    Subindo mais alterações

--- RestAPILearning/RestAPILearning/Controllers/CalculadoraController.cs
+++ RestAPILearning/RestAPILearning/Controllers/CalculadoraController.cs
@@ -1,11 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace RestAPILearning.Controllers
+namespace ApiCalculator.Controllers
 {
     [ApiController]
     [Route("[controller]")]
--- RestAPILearning/RestAPILearning/Program.cs
+++ RestAPILearning/RestAPILearning/Program.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace RestAPILearning
+namespace ApiCalculator
 {
     public class Program
     {
